037f111e83f37c6f8ae40d7ff5cc2b899494fb65
Oleg Musijenko <45047700+Poselsky@users.noreply.github.com>
2020-07-29 13:53:42 +0200

    Add winforms and draw board

---
+++ WinFormsChess/Chess.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WinFormsChess
+{
+    partial class Chess
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainCanvas = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.MainCanvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MainCanvas
+            // 
+            this.MainCanvas.Location = new System.Drawing.Point(42, 26);
+            this.MainCanvas.Name = "MainCanvas";
+            this.MainCanvas.Size = new System.Drawing.Size(700, 700);
+            this.MainCanvas.TabIndex = 0;
+            this.MainCanvas.TabStop = false;
+            // 
+            // Chess
+            // 
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(782, 753);
+            this.Controls.Add(this.MainCanvas);
+            this.Name = "Chess";
+            this.Text = "Chess";
+            this.Load += new System.EventHandler(this.Chess_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.MainCanvas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox MainCanvas;
+    }
+}
+
---
+++ WinFormsChess/Chess.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsChess
+{
+    public partial class Chess : Form
+    {
+        public Chess()
+        {
+            InitializeComponent();
+            InitializeBoard();
+        }
+
+        private void InitializeBoard()
+        {
+            int width = MainCanvas.Width;
+            int height = MainCanvas.Height;
+            Image MainBitmap = new Bitmap(MainCanvas.Width, MainCanvas.Height);
+
+            using (Graphics transformer = Graphics.FromImage(MainBitmap))
+                for(int i = 0; i < 8; i++)
+                    for(int j = 0; j < 8; j++)
+                    {
+                        if((i+j)%2 == 0)
+                            transformer.FillRectangle(Brushes.White, new Rectangle(width/8 * i, height/8 * j, width/8, height/8));
+                        else
+                            transformer.FillRectangle(Brushes.SandyBrown, new Rectangle(width/8 * i, height/8 * j, width / 8, height / 8));
+
+                    }                
+
+            MainCanvas.Image = MainBitmap;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Chess_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
